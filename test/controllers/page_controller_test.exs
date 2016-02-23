ead4dd212bd40a0af2316d26d5e160b1f03c3b68
Curtis Schiewek <curtis.schiewek@gmail.com>
2016-02-23 13:38:23 -0500

    fix test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Plops.PageControllerTest do
 
   test "GET /" do
     conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Plops"
   end
 end
