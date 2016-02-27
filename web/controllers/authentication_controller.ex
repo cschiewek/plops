54daed2cf73dade18f6e047e3276331696ed4c8e
Curtis Schiewek <curtis.schiewek@gmail.com>
2016-02-27 09:24:49 -0500

    switch intro to video, put details of auth fails

--- web/controllers/authentication_controller.ex
+++ web/controllers/authentication_controller.ex
@@ -15,9 +15,9 @@ defmodule Plops.AuthenticationController do
     |> redirect(to: "/")
   end
 
-  def callback(%{ assigns: %{ ueberauth_failure: _fails } } = conn, _params) do
+  def callback(%{ assigns: %{ ueberauth_failure: fails } } = conn, _params) do
     conn
-    |> put_flash(:error, "Failed to authenticate.")
+    |> put_flash(:error, "Failed to authenticate. #{fails}")
     |> redirect(to: "/")
   end
 
